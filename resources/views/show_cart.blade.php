271355621bbe40d907a0d0fd5df219e584cfe91f
JhorgiHisamawa <jhorgisukangupil@gmail.com>
2025-03-24 21:20:40 +0700

    fix: fix image url on show cart

--- resources/views/show_cart.blade.php
+++ resources/views/show_cart.blade.php
@@ -14,7 +14,7 @@
     @endif
 
     @foreach ($carts as $cart)
-    <img src="{{ url('storage/app/public/' . $cart->product->image_url) }}" alt="" height="100px">
+    <img src="{{ url('storage/' . $cart->product->image_url) }}" alt="" height="100px">
     <p>Name: {{$cart->product->name}}</p>
     <br>
     <form action="{{ route('update_cart', $cart) }}" method="post">
