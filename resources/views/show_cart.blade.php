d95c4d440a6e05cbd02510c06e39eeec8652fd6b
JhorgiHisamawa <jhorgisukangupil@gmail.com>
2025-03-24 23:20:48 +0700

    features: add delete cart api

--- resources/views/show_cart.blade.php
+++ resources/views/show_cart.blade.php
@@ -23,6 +23,11 @@
         <input type="number" name="amount" value="{{$cart->amount}}">
         <button type="submit">Update</button>
         </form>
+        <form action="{{ route('delete_cart', $cart) }}" method="post">
+            @method('delete')
+            @csrf
+            <button type="submit">Delete</button>
+        </form>
     @endforeach
 </body>
 </html>
